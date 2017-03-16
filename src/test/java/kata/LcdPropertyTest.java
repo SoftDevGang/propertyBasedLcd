10116780f3e216473eb958176f63db80738345fa
Thomas Sundberg <tsu@kth.se>
2017-03-16 17:57:45 +0100

    Added test for the bottom, middle

--- src/test/java/kata/LcdPropertyTest.java
+++ src/test/java/kata/LcdPropertyTest.java
@@ -3,7 +3,6 @@ package kata;
 import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.InRange;
-import com.pholser.junit.quickcheck.generator.java.lang.IntegerGenerator;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.fest.assertions.api.Assertions;
 import org.junit.Assume;
@@ -35,7 +34,7 @@ public class LcdPropertyTest {
     // etc for all places have it / don't have it
 
     @Property
-    public void allDigitsHaveThreeLines( @InRange(min = "0") Integer arg) {
+    public void allDigitsHaveThreeLines(@InRange(min = "0") Integer arg) {
         String displayString = new Lcd().getDisplayString(arg);
         String[] lines = displayString.split(Lcd.NL);
         Assertions.assertThat(lines.length).isEqualTo(3);/**/
@@ -43,15 +42,30 @@ public class LcdPropertyTest {
 
     @Property(maxShrinkDepth = 500, maxShrinks = 500)
     public void oneAndFourDontHaveUnderscoreOnTopMiddleColumnBuiltIn(@InRange(minInt = 0) int arg) {
-        Assume.assumeTrue(doesNotContain1nor4(arg));
+        Assume.assumeTrue(doesNotContain(arg, "1", "4"));
         String displayString = new Lcd().getDisplayString(arg);
         String[] lines = displayString.split(Lcd.NL);
         String firstLine = lines[0];
         Assertions.assertThat(firstLine).matches("( _ )+");
     }
 
-    private boolean doesNotContain1nor4(int arg) {
-        return !String.valueOf(arg).contains("1") && !String.valueOf(arg).contains("4");
+    @Property(trials = 1000, maxShrinkDepth = 500, maxShrinks = 500) // One thousand to find enough examples values
+    public void any_number_not_1_4_7_9_have_underscores_on_top_bottom_column(@InRange(minInt = 0) int arg) {
+        Assume.assumeTrue(doesNotContain(arg, "1", "4", "7", "9"));
+        String displayString = new Lcd().getDisplayString(arg);
+        String[] lines = displayString.split(Lcd.NL);
+        String bottomLine = lines[2];
+        Assertions.assertThat(bottomLine).matches("([| ]_[| ])+");
+    }
+
+    private boolean doesNotContain(int arg, String... illegal) {
+        for (String digit : illegal) {
+            if (String.valueOf(arg).contains(digit)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
